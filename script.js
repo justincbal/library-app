e8802b2a9a5f38f7eda831cdacab07c33ca22db2
justincbal <justinseebee@hotmail.com>
2022-11-12 01:50:15 -0500

    finish rough

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const btn = document.querySelector('#btn');
-
-
-// btn.addEventListener('click', (e) => {
-//     e.preventDefault();
-
-// })
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const read = document.querySelector('#read');
 
 btn.addEventListener('click', addBookToLibrary);
 
@@ -23,15 +21,51 @@ function Book(title, author, pages, completed) {
 
 function addBookToLibrary() {
     event.preventDefault();
-    const title = document.querySelector('#title');
-    const author = document.querySelector('#author');
-    const pages = document.querySelector('#pages');
-    const read = document.querySelector('#read');
-
-    const book = new Book(title, author, pages, read);
+    const book = new Book(title.value, author.value, pages.value, read.checked);
     myLibrary.push(book);
 
-    console.log(book);
-    console.log(myLibrary);
+    
+    addToList(book);
+
+    // Resets form fields
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+    read.checked = false;
+
 }
 
+function addToList(item) {
+    const listings = document.querySelector('.listings');
+    const listRow = document.createElement('div');
+    listRow.classList.add('list-row');
+
+    const itemTitle = document.createElement('div');
+    const itemAuth = document.createElement('div');
+    const itemPages = document.createElement('div');
+    const itemRead = document.createElement('div');
+
+    itemTitle.textContent = item.title;
+    itemAuth.textContent = item.author;
+    itemPages.textContent = item.pages;
+    
+    item
+
+    if (item.completed === true) {
+        itemRead.textContent = "COMPLETED";
+    }
+    else {
+        itemRead.textContent = "INCOMPLETE";
+    }
+
+    // Append to listRow and listing
+    listRow.append(itemTitle);
+    listRow.append(itemAuth);
+    listRow.append(itemPages);
+    listRow.append(itemRead);
+
+    listings.append(listRow);
+
+
+    
+}
